0265b9b97700aae6cc316807ca651207a23d80d3
Daphnia <daphnia@users.noreply.github.com>
2017-12-18 22:47:42 +0200

    The final assingment completed

--- data/create_final.R
+++ data/create_final.R
@@ -5,29 +5,43 @@
 
 setwd("~/Github/IODS-final")
 
-#I will use Boston dataset for R package called MASS.
-
 library(MASS)
 library(dplyr)
 library(tidyr)
 library(ggplot2)
 
+data("Boston")
+str(Boston)
+
+#The variable chas (Charles River dummy variable which is 1 if tract 
+#bounds river and 0 otherwise) is integer and is removed from the analysis:
+
+Boston <- dplyr::select(Boston, -chas)
+
+# Note1: 
+# rad (index of accessibility to radial highways) is integer 
+# and others are numerical. 
+# Note2: 
+# The variable black gets smaller values as the proportion of black skin 
+# colour increases.
 
 
-Boston_final <- data("Boston")
-str(Boston_final)
+# Now, as I have decided to focus on 25 % of the towns 
+# that has the most highest crime rates, I remove other observations:  
+crim4 <- Boston$crim
+bins <- quantile(crim4)
+bins
+#Then I keep those observations where crim > 3.677083. 
+boston_final <- subset(Boston, (crim > 3.677083))
 
-######IMPORTANT: Make clear comments if changes are made!
-
-
-
+summary(boston_final)
 
 #Finished wrangling:
-write.csv(Boston_final, file = "data/Boston.csv", row.names = FALSE, quote = TRUE, eol = "\n", fileEncoding = "UTF-8")
+write.csv(boston_final, file = "data/boston_final.csv", row.names = FALSE, quote = TRUE, eol = "\n", fileEncoding = "UTF-8")
 #reading test:
-read_test <- read.csv("data/Boston.csv", header = TRUE)
+read_test <- read.csv("data/boston_final.csv", header = TRUE)
 str(read_test)
-str(alc)
+str(boston_final)
 head(read_test)
-head(alc)
+head(boston_final)
 remove(read_test)
